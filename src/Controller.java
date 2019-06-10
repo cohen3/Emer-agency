46f9aadbb4702e24a11af857a3e758f65117b549
elad <eladc5492@gmail.com>
2019-06-10 20:50:45 +0300

    added JDBS jar and some classes

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,9 @@
 public class Controller {
+    // Usecases we will implement:
+    //
+    // 1) UC1: add update to an event
+    // 2) UC2: view update history
+    // 3) UC3: publish an event
     public static void main(String[] args) {
 
     }
--- src/Organization.java
+++ src/Organization.java
@@ -1,14 +1,15 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 
 public class Organization {
 
     User admin;
     HashMap<Integer, User> users;
+    HashSet<Event> events;
 
     public Organization(User admin) {
         this.admin = admin;
+        this.events = new HashSet<>();
         this.users = new HashMap<>();
         users.put(this.admin.getID(), this.admin);
     }
